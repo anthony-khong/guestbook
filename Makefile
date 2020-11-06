76acd1151bd85625cd1b25af90653785af07cd3b
anthony-khong <anthony.kusumo.khong@gmail.com>
2020-11-06 11:53:35 +0700

    Created and passed simple CI step

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+kondo:
+	clj-kondo --lint src test --cache false
+
+fmt:
+	lein cljfmt check
+
+ancient:
+	lein ancient
+
+ci: kondo fmt ancient
--- test/clj/guestbook/db/core_test.clj
+++ test/clj/guestbook/db/core_test.clj
@@ -1,14 +1,14 @@
 (ns guestbook.db.core-test
   (:require
+   [clojure.test :as t]
+   [guestbook.config :refer [env]]
    [guestbook.db.core :refer [*db*] :as db]
    [java-time.pre-java8]
    [luminus-migrations.core :as migrations]
-   [clojure.test :refer :all]
-   [next.jdbc :as jdbc]
-   [guestbook.config :refer [env]]
-   [mount.core :as mount]))
+   [mount.core :as mount]
+   [next.jdbc :as jdbc]))
 
-(use-fixtures
+(t/use-fixtures
   :once
   (fn [f]
     (mount/start
@@ -17,22 +17,22 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+(t/deftest test-users
   (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-    (is (= 1 (db/create-user!
-              t-conn
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"}
-              {})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"} {})))))
+    (t/is (= 1 (db/create-user!
+                t-conn
+                {:id         "1"
+                 :first_name "Sam"
+                 :last_name  "Smith"
+                 :email      "sam.smith@example.com"
+                 :pass       "pass"}
+                {})))
+    (t/is (= {:id         "1"
+              :first_name "Sam"
+              :last_name  "Smith"
+              :email      "sam.smith@example.com"
+              :pass       "pass"
+              :admin      nil
+              :last_login nil
+              :is_active  nil}
+             (db/get-user t-conn {:id "1"} {})))))
