3f9b79d7ab6750a184c7ca1b0d6b45a516e7ced6
anthony-khong <anthony.kusumo.khong@gmail.com>
2020-11-06 22:13:18 +0700

    Added more Kondo config

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,15 +1,15 @@
 (ns user
   "Userspace functions you can run by default in your local REPL."
   (:require
-   [guestbook.config :refer [env]]
    [clojure.pprint]
    [clojure.spec.alpha :as s]
-   [expound.alpha :as expound]
-   [mount.core :as mount]
-   [guestbook.core :refer [start-app]]
-   [guestbook.db.core]
    [conman.core :as conman]
-   [luminus-migrations.core :as migrations]))
+   [expound.alpha :as expound]
+   [guestbook.config :refer [env]]
+   [guestbook.core]
+   [guestbook.db.core]
+   [luminus-migrations.core :as migrations]
+   [mount.core :as mount]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
@@ -60,4 +60,5 @@
   [name]
   (migrations/create name (select-keys env [:database-url])))
 
-
+(comment
+  (start))
